c71f0a4bf3ea8bb22a4a74c77302ea7c1e99b52b
David Tengdin and Jacob Maine <pair+dtengdin+jacob@pivotallabs.com>
2013-04-10 17:07:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BytesAtoms::Application.config.secret_token = '3cc1f21e92f5fdf3eef6717f6957540aa39c3617aa2e3d877d65a11c3381394872224b13dcf73eefdf3c74343abe913550389824f0a10d9e47dbbfdb06d235d2'
