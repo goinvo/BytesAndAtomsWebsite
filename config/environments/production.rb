a209b07c488a33f0b8f039865426c19c00ef9142
David Tengdin and Jacob Maine <pair+dtengdin+jacob@pivotallabs.com>
2013-04-15 12:01:11 -0400

    Fix asset precompilation
    Two problems: there was some invalid SCSS, and we were linking directly
    to stylesheets, not having Rails find us the right ones.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ BytesAtoms::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( index.css groundworkcss/groundwork.css groundworkcss/groundwork-ie.css groundworkcss/no-svg.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
