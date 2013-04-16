d558217ab2920e70cb2c5cc39968386c1a2efd50
David Tengdin and Jacob Maine <pair+dtengdin+jacob@pivotallabs.com>
2013-04-16 17:28:28 -0400

    refactor blog section to fade picture

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -1,8 +1,3 @@
-BytesAndAtoms = {
-  start: function() {
-  }
-};
-
 (function ($) {
   $.fn.showHide = function (options) {
     //default vars for the plugin
@@ -11,16 +6,28 @@ BytesAndAtoms = {
       easing: ''
     };
     var options = $.extend(defaults, options);
+    var divs = $('.toggleDiv')
+    divs.css("position", "absolute");
+    divs.hide();
+    $(divs[0]).show();
+
 
     $(this).click(function () {
       // add the class .toggleDiv to each div you want to automatically close
       $('.toggleDiv').fadeOut(options.speed, options.easing);
       // this reads the rel attribute of the button to determine which div id to toggle
-      var toggleDiv = $(this).attr('rel');
+      var toggleDiv = $(this).attr('href');
       // here we toggle show/hide the correct div at the right speed and using which easing effect
       $(toggleDiv).fadeIn(options.speed, options.easing);
       return false;
     });
   };
 })(jQuery);
+
+BytesAndAtoms = {
+  start: function() {
+    $('.show_hide').showHide();
+    $('#navigation').scrollToFixed( { marginTop: 0, limit: 40000 } );
+  }
+};
 
